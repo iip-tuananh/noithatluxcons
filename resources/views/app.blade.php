c560899b35b3bac3542cd4f631b0b73af4865814
vũ nguyễn tiến <nguyentienvu4897@gmail.com>
2022-11-28 09:12:25 +0700

    fixed

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -21,7 +21,7 @@
 
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <script>
-      window.__ENV__ = window.__ENV || {}; __ENV__.link ='http://luxcon.vn/';
+      window.__ENV__ = window.__ENV || {}; __ENV__.link ='https://luxcon.vn/';
       window.Laravel = {!! 
           json_encode([
               'csrf_token' => csrf_token(),
